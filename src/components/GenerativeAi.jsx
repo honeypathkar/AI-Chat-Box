59cd0fcb9b5e34d0a2b79b5d4bba6c0c6ef6a955
honeypatkar <honeypatkar70@gmail.com>
2024-06-26 13:58:28 +0530

    Changing css and deleting react svg

--- src/components/GenerativeAi.jsx
+++ src/components/GenerativeAi.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import Spinner from "./Spinner";
 import "../App.css";
-import SendIcon from '@mui/icons-material/Send';
-
+import SendIcon from "@mui/icons-material/Send";
 
 export default function GenerativeAi() {
   const [answer, setAnswer] = useState("");
@@ -35,26 +34,25 @@ export default function GenerativeAi() {
 
   return (
     <div className="container mt-10">
-      <div className="text-center text-3xl"> Welcome to SI ( Smart Intelligence )</div>
+      <div className="text-center text-3xl">
+        {" "}
+        Welcome to SI ( Smart Intelligence )
+      </div>
       <div className="flex justify-center">
-
-      {!loading && (
-        <pre
-          className="mt-5 answer"
-          dangerouslySetInnerHTML={{ __html: answer.replaceAll("*", " ") }}
-        ></pre>
-      )}
+        {!loading && (
+          <pre className="mt-5 answer">{answer.replaceAll("*", " ").replaceAll("#", "")} </pre>
+        )}
       </div>
       <form onSubmit={handleSubmit} className="flex promptBox container">
         <input
           type="search"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
-          className="searchBox rounded-full py-2 pl-4 border"
+          className="searchBox rounded-full py-2 px-4 border"
           placeholder="Enter text for generate your answer... "
         />
-        <button type="submit" className="btn rounded-full border">
-          <SendIcon sx={{color: "#FFF"}}/>
+        <button type="submit" className="btn rounded-full border ml-1 bg-[#121223] px-3 py-2">
+          <SendIcon sx={{ color: "#FFF" }} />
         </button>
       </form>
       {loading && <Spinner />}
